1218519a90a3a719d451710940109f7cb19a5953
pascal-moreno <coach.pascalmoreno@gmail.com>
2022-12-04 00:36:22 +0100

    change-logo

--- pages/index.js
+++ pages/index.js
@@ -12,18 +12,20 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-      <p> Hello Bro - depuis Bouges</p>
+      <p>  <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={1172} height={1116} />
+          </span></p>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://www.pascal-moreno.com"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image src="/vercel.svg" alt="Vercel Logo" width={172} height={116} />
           </span>
         </a>
       </footer>
