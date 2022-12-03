4e2ffb274157911cf34fe47f748b54b0b53ead15
pascal-moreno <coach.pascalmoreno@gmail.com>
2022-12-03 12:59:16 +0100

    add-surname

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'Pascal Moreno' })
+  res.status(200).json({ name: 'Pascal Moreno' , surname: 'Zenbox'})
 }
